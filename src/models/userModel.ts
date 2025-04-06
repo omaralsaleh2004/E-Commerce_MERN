023ccaca4129b80c9ef204ccdda061666d65a9d8
Omar <Omar@example.com>
2025-04-06 23:25:19 +0300

    Added Document from mongoose

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-export interface IUser {
+export interface IUser extends Document {
   firstName: string;
   lastName: string;
   email: string;
