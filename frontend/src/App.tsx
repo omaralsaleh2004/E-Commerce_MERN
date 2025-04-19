3d60399fe7e7514aa8d22d56cfe307e09a0d3c14
Omar <Omar@example.com>
2025-04-19 19:06:55 +0300

    Cart Context + Cart Provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,24 @@ import AuthProvider from "./Context/Auth/AuthProvider";
 import LoginPage from "./Pages/LoginPage";
 import CartPage from "./Pages/CartPage";
 import ProtectedRoute from "./Components/ProtectedRoute";
+import CartProvider from "./Context/Cart/CartProvider";
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/cart" element={<CartPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/cart" element={<CartPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </AuthProvider>
   );
 }
--- frontend/src/Components/ProductCard.tsx
+++ frontend/src/Components/ProductCard.tsx
@@ -1,35 +1,42 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useCart } from "../Context/Cart/CartContext";
 
 interface Props {
-    title: string;
-    price: number;
-    image: string;
-    _id: string;
+  title: string;
+  price: number;
+  image: string;
+  _id: string;
 }
 
-export default function ProductCard({image ,price ,title}:Props) {
+export default function ProductCard({ _id, image, price, title }: Props) {
+  const { addItemToCart } = useCart();
   return (
     <Card>
-      <CardMedia
-        sx={{ height: 200 }}
-        image= {image}
-        title="green iguana"
-      />
+      <CardMedia sx={{ height: 200 }} image={image} title="green iguana" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary', fontSize : "22px"}}>
-         ${price}
+        <Typography
+          variant="body2"
+          sx={{ color: "text.secondary", fontSize: "22px" }}
+        >
+          ${price}
         </Typography>
       </CardContent>
-      <CardActions sx={{pt : "25px"}}>
-        <Button variant='contained' size="medium">Add to Cart</Button>
+      <CardActions sx={{ pt: "25px" }}>
+        <Button
+          variant="contained"
+          size="medium"
+          onClick={() => addItemToCart(_id)}
+        >
+          Add to Cart
+        </Button>
       </CardActions>
     </Card>
   );
