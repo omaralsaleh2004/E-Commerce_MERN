1a9a7470ffd733d15f46391945137cf1ac84c4ee
Omar <Omar@example.com>
2025-04-21 15:05:49 +0300

    Cart Page layout

--- frontend/src/Pages/CartPage.tsx
+++ frontend/src/Pages/CartPage.tsx
@@ -1,6 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
-import { useState } from "react";
-import { useAuth } from "../Context/Auth/AuthContext";
+import { Box, Button, ButtonGroup, Container, Typography } from "@mui/material";
 import { useCart } from "../Context/Cart/CartContext";
 
 const CartPage = () => {
@@ -8,10 +6,58 @@ const CartPage = () => {
 
   return (
     <Container sx={{ mt: 4 }}>
-      <Typography variant="h4">My Cart</Typography>
-      {cartItems.map((item) => (
-        <Box>{item.title}</Box>
-      ))}
+      <Typography variant="h4" textAlign={"center"}>
+        My Cart
+      </Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 3, mt: 3 }}>
+        {cartItems.map((item) => (
+          <Box
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            sx={{
+              border: 1,
+              borderColor: "#f2f2f2",
+              borderRadius: 5,
+              padding: 2,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 2,
+              }}
+            >
+              <img width={130} src={item.image} alt="product image" />
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <Typography variant="h5">{item.title}</Typography>
+                <Typography>
+                  {item.quantity} x ${item.unitPrice}{" "}
+                </Typography>
+                <Button>Remove Item</Button>
+              </Box>
+            </Box>
+            <ButtonGroup variant="contained" aria-label="Basic button group">
+              <Button>-</Button>
+              <Button>+</Button>
+            </ButtonGroup>
+          </Box>
+        ))}
+        <Box>
+          <Typography variant="h4" textAlign={"center"}>
+            Total Amount : ${totalAmount}
+          </Typography>
+        </Box>
+      </Box>
     </Container>
   );
 };
